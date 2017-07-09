d5ef7e2e08fb2c0a7b2272ecdaf8975eb57fef74
hillguo <hillguo@tencent.com>
2017-07-09 16:18:24 +0800

    修改 使用token 机制验证 用户合法性

--- handler/handler.py
+++ handler/handler.py
@@ -25,11 +25,6 @@ class BaseHandler(tornado.web.RequestHandler):
     def db(self):
         return self.application.db
 
-    def write_miss_argument(self):
-        res={}
-        res["code"] = 1
-        res["message"] = "Miss arguments"
-        self.write(res)
 
     def generate_token(self):
         while True:
@@ -39,18 +34,27 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def set_token(self,token,userid):
         self.__TOKEN_LIST[token] = userid
-        self.set_secure_cookie('_token', token)
+        #self.set_secure_cookie('_token', token)
         
 
     def get_current_user(self):
-        token = self.get_secure_cookie("_token").decode()
+        #token = self.get_secure_cookie("_token").decode()
+        token = self.get_argument("token",None)
         if token and token in self.__TOKEN_LIST:
             userid = self.__TOKEN_LIST[token]
             return userid
         return None
+    
+    def valid_user(self):
+        token = self.get_argument("token",None)
+        user_id = self.get_argument("user_id",None)
+        if token and user_id and token in self.__TOKEN_LIST:
+            if user_id == self.__TOKEN_LIST[token]:
+                return True
+        return False
+            
 
 class IndexHandler(BaseHandler):
-    @tornado.web.authenticated
     def get(self):
         sql=text("show tables")
         res=self.db.execute(sql).fetchall()
@@ -58,7 +62,7 @@ class IndexHandler(BaseHandler):
 
 
 class LoginHandler(BaseHandler):
-    def get(self):
+    def post(self):
         user_id = self.get_argument("user_id",None)
         if user_id == None :
             self.write_miss_argument()
@@ -70,29 +74,39 @@ class LoginHandler(BaseHandler):
             self.db.commit()
         new_token = self.generate_token()
         self.set_token(new_token,user_id)
-        self.write("ok")
+        res ={}
+        res["code"] = 0
+        res["token"] = new_token
+        res["msg"] = "login success !"
+        self.write(res)
 
-class Sample(BaseHandler):
+class UserInfoHandler(BaseHandler):
     '''例子：1. 所有处理类继承 BaseHandle
              2. get 处理get 请求 , post 方法 处理 post 请求
              3. self.get_argument("arg_name",None) 获取传来的参数，没有返回None
-             4. 所有 有可能被 越权 的操作，都需要在 加@tornado.web.authenticated 验证登录
+             4. 所有 有可能被 越权 的操作，都需要 valid_user() // 会验证 userid 和token 是否一致
              5. 返回值均为json
              6. 例子： 根据请求的 user_id 返回个人信息. (紧作参考，不严禁)
     '''
-    @tornado.web.authenticated
     def post(self):
-        user_id = self.get_argument("user_id",None)
-        if user_id != self.current_user:
-            self.write("越权操作")
-            return
-        if user_id :
-            user = self.db.query(User).filter(User.user_qq_id == user_id).first()
-            res={}
-            res["user_id"]=user.user_qq_id
-            res["user_name"]=user.user_name
-            res["user_pic"] = user.user_pic
+        if not self.valid_user():
+            res ={}
+            res["code"] = -1
+            res["msg"] = "please login again!"
             self.write(res)
-        else :
-            self.write("error")
-        
+        else:
+            user_id = self.get_argument("user_id",None)
+            user = self.db.query(User).filter(User.user_qq_id == user_id).first()
+            if user :
+                res ={}
+                res["user_id"]=user.user_qq_id
+                res["user_name"]=user.user_name
+                self.write(res)
+
+
+
+
+
+
+
+
--- route/route.py
+++ route/route.py
@@ -2,5 +2,6 @@ from tornado.web import url
 from handler.handler import *
 routes = [
         url(r"/", IndexHandler, name='index'),
-        url(r"/login", LoginHandler)
+        url(r"/login", LoginHandler),
+        url(r"/userinfo",UserInfoHandler)
         ]
