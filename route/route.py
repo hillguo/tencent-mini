e88b2349890463d41810f7ee51db96258e67c37e
hillguo <hillguo@tencent.com>
2017-07-10 21:17:08 +0800

    重新提交吧

--- route/route.py
+++ route/route.py
@@ -1,10 +1,9 @@
 from tornado.web import url
-from handler.handler import *
 from handler.login_handler import LoginHandler
 routes = [
         url(r"/login", LoginHandler),
-        url(r"/song",SongNearHandler)
-		url(r"/story", StoryHandler, name='story'),
-		url(r"/story/([0-9]+)/up", PraiseHandler, name='praisestory'),
-		url(r"/story/([0-9]+)/comment", name='getcomment')
+        #url(r"/song",SongNearHandler),
+		#url(r"/story", StoryHandler, name='story'),
+		#url(r"/story/([0-9]+)/up", PraiseHandler, name='praisestory'),
+		#url(r"/story/([0-9]+)/comment", name='getcomment')
         ]
