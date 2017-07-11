5994a0aa559aa49f3b4e55f229b751b59d3ba8fb
jun-wen <894607785@qq.com>
2017-07-11 22:28:40 +0800

    更新接口返回的json格式

--- route/route.py
+++ route/route.py
@@ -9,6 +9,6 @@ routes = [
 		#url(r"/story/([0-9]+)/up", PraiseHandler, name='praisestory'),
 		#url(r"/story/([0-9]+)/comment", name='getcomment'),
         url(r"/userinfo",UserInfoHandler),
-        url(r"/song", SongNearHandler),
+        url(r"/song/near", SongNearHandler),
         url(r"/song/story", SongStoryHandler)
         ]
