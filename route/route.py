832a7978ddf35886f634912aaf0af18e4f25f68c
yuchajianshi <491574452@qq.com>
2017-07-11 10:54:22 +0800

    添加了留故事，给故事点赞，获取故事评论3个API

--- route/route.py
+++ route/route.py
@@ -2,12 +2,13 @@ from tornado.web import url
 from handler.login_handler import LoginHandler, UserInfoHandler
 from handler.near_song_handler import SongNearHandler
 from handler.song_story_handler import SongStoryHandler
+from handler.story_handler import StoryHandler, PraiseHandler, CommentHandler
 routes = [
         url(r"/login", LoginHandler),
         #url(r"/song",SongNearHandler),
-		#url(r"/story", StoryHandler, name='story'),
-		#url(r"/story/([0-9]+)/up", PraiseHandler, name='praisestory'),
-		#url(r"/story/([0-9]+)/comment", name='getcomment'),
+		url(r"/story", StoryHandler, name='story'),
+		url(r"/story/([0-9]+)/up", PraiseHandler, name='praisestory'),
+		url(r"/story/([0-9]+)/comment", CommentHandler, name='getcomment'),
         url(r"/userinfo",UserInfoHandler),
         url(r"/song", SongNearHandler),
         url(r"/song/story", SongStoryHandler)
