26d0a9e752ad795420d26d5eedb954b1929bd18e
zhuangqh <zhuangqhc@163.com>
2017-07-13 09:56:43 +0800

    取消数据的硬编码连接

--- handler/story_handler.py
+++ handler/story_handler.py
@@ -1,130 +1,83 @@
 import json
 from handler.base_handler import BaseHandler
-import pymysql
+from datetime import datetime 
+import logging
 
 class StoryHandler(BaseHandler):
     def post(self):
         "留下故事信息"
-        postdata = self.request.body.decode('utf-8')
-        postdata = json.loads(postdata)
-        db = pymysql.connect("595f58f641420.gz.cdb.myqcloud.com", "cdb_outerroot", "mini123456", "tingwen", 5880, use_unicode=True, charset="utf8")
-        cursor = db.cursor()
-        cursor.execute("SET NAMES utf8")
-        cursor.execute("SET CHARACTER SET utf8")
-        cursor.execute("SET character_set_connection=utf8")
+        body = json.loads(self.request.body)
+
+        content = body["content"]
+        song_id = body["song_id"]
+        longitude = body["longtitude"]
+        latitude = body["latitude"]
+        user_id = body["user_id"]
         sql = '''
-		    INSERT INTO story(content, song_id, praisenum, longitude, latitude, time, user_id)
-			VALUES("{0}", {1}, 0, {2}, {3}, "{4}", "{5}")
-        '''.format(postdata['content'], postdata['song_id'], postdata['longitude'], postdata['latitude'], postdata['time'], postdata['user_id'])
+		    INSERT INTO story(content, song_id, up, longitude, latitude, created_at, user_id)
+			VALUES("{0}", {1}, 0, {2}, {3}, "{4}", {5})
+        '''.format(content, song_id, longitude, latitude, datetime.utcnow(), user_id)
         try:
-            cursor.execute(sql)
-            db.commit()
+            self.db.execute(sql)
+            self.db.commit()
             info = {'code': 0, 'errinfo': 'OK'}
-            info_json = json.dumps(info)
-            info_encode = info_json.encode('utf-8')
-            self.write(info_encode)
-        except:
-            db.rollback()
+            self.write(info)
+        except Exception as e:
+            logging.error(e)
+            self.db.rollback()
             err = {'code': 1, 'errinfo': '存入故事失败'}
-            err_json = json.dumps(err)
-            err_encode = err_json.encode('utf-8')
-            self.write(err_encode)
-        db.close()
-			
+            self.write(err)
+
 class PraiseHandler(BaseHandler):
     def post(self, story_id):
         "给故事点赞"
         postdata = self.request.body.decode('utf-8')
+        if not postdata:
+            err = {'code': 3, 'errinfo': 'post数据为空'}
+            self.write(err)
+            return
         postdata = json.loads(postdata)
-        db = pymysql.connect("595f58f641420.gz.cdb.myqcloud.com", "cdb_outerroot", "mini123456", "tingwen", 5880, use_unicode=True, charset="utf8")
-        cursor = db.cursor()
-        cursor.execute("SET NAMES utf8")
-        cursor.execute("SET CHARACTER SET utf8")
-        cursor.execute("SET character_set_connection=utf8")
+
         sql = '''
             SELECT * FROM likestory WHERE user_id = "{0}"
         '''.format(postdata['user_id'])
         try:
-            cursor.execute(sql)
-        except:
+            results = self.db.execute(sql).fetchall()
+        except Exception as e:
             err = {'code': 1, 'errinfo': '查询用户是否喜欢过该首歌曲时失败'}
-            err_json = json.dumps(err)
-            err_encode = err_json.encode('utf-8')
-            self.write(err_encode)
-            db.close()
+            self.write(err)
+            logging.error(e)
             return
-        results = cursor.fetchall()
+  
         if len(results) != 0:
             err = {'code': 2, 'errinfo': '已经点过赞了'}
-            err_json = json.dumps(err)
-            err_encode = err_json.encode('utf-8')
-            self.write(err_encode)
-            db.close()
+            self.write(err)
             return
-			
+
         sql = '''
             INSERT INTO likestory(user_id, story_id) VALUES("{0}", {1})
         '''.format(postdata['user_id'], story_id)
         try:
-            cursor.execute(sql)
-            db.commit()
-        except:
-            db.rollback()
+            self.db.execute(sql)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
             err = {'code': 3, 'errinfo': '"用户喜欢故事"信息存入失败'}
-            err_json = json.dumps(err)
-            err_encode = err_json.encode('utf-8')
-            self.write(err_encode)
-            db.close()
+            self.write(err)
+            logging.error(e)
             return
-		
+
         sql = '''
-            UPDATE story SET praisenum = praisenum + 1 WHERE story_id = {0}
+            UPDATE story SET up = up + 1 WHERE story_id = {0}
         '''.format(story_id)
         try:
-            cursor.execute(sql)
-            db.commit()
+            self.db.execute(sql)
+            self.db.commit()
             info = {'code': 0, 'errinfo': 'OK'}
-            info_json = json.dumps(info)
-            info_encode = info_json.encode('utf-8')
-            self.write(info_encode)
-        except:
-            db.rollback()
+            self.write(info)
+        except Exception as e:
+            self.db.rollback()
             err = {'code': 4, 'errinfo': '更新点赞数失败'}
-            err_json = json.dumps(err)
-            err_encode = err_json.encode('utf-8')
-            self.write(err_encode)
-        db.close()
-        
-class CommentListHandler(BaseHandler):
-    def get(self, story_id):
-        "获取故事的评论等信息"
-        db = pymysql.connect("595f58f641420.gz.cdb.myqcloud.com", "cdb_outerroot", "mini123456", "tingwen", 5880, use_unicode=True, charset="utf8")
-        cursor = db.cursor()
-        cursor.execute("SET NAMES utf8")
-        cursor.execute("SET CHARACTER SET utf8")
-        cursor.execute("SET character_set_connection=utf8")
-        #获取故事ID，评论内容，点赞数，时间
-        sql = '''
-            SELECT comment_id, content, praisenum, time FROM comment WHERE story_id = {0}
-        '''.format(story_id)
-        try:
-            cursor.execute(sql)
-            db.commit()
-        except:
-            db.rollback()
-            err = {'code': 1, 'errinfo': '获取评论失败'}
-            err_json = json.dumps(err)
-            err_encode = err_json.encode('utf-8')
-            self.write(err_encode)
-            db.close()
+            self.write(err)
+            logging.error(err)
             return
-        results = cursor.fetchall()
-        db.close() 
-        allstoryinfo = []
-        for row in results:
-            singlestoryinfo	= {'comment_id': row[0], 'content': row[1], 'praisenum': row[2], 'time': row[3]}
-            allstoryinfo.append(singlestoryinfo)
-        info = {'code': 0, 'data': allstoryinfo, 'errinfo': 'OK'}
-        storyinfo_json = json.dumps(info)
-        storyinfo_encode = storyinfo_json.encode('utf-8')
-        self.write(storyinfo_encode)
